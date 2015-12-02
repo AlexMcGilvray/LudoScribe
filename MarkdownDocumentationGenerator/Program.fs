6add5435d70c992f7ad6acbd858152f4958fd223
Alex McGilvray <alex@levelism.com>
2015-12-01 23:21:59 -0800

    Copied in some example code from the FSharp.Markdown documentation for usage reference. The program spits out an output.txt file that contains a blurb from that tutorial in html form.

--- MarkdownDocumentationGenerator/Program.fs
+++ MarkdownDocumentationGenerator/Program.fs
@@ -3,12 +3,25 @@
 open CommandLineOptions
 open FSharp.Markdown
 open System
+open System.IO
 
+let testDocument = """
+# F# Hello world
+Hello world in [F#](http://fsharp.net) looks like this:
 
+    printfn "Hello world!"
+
+For more see [fsharp.org][fsorg].
+
+  [fsorg]: http://fsharp.org "The F# organization." """
+
+let parsed = Markdown.Parse(testDocument) 
+let html = Markdown.WriteHtml(parsed)  
 
 [<EntryPoint>]
 let main argv = 
     let filteredArgs = argv.Length 
+    File.WriteAllText("output.txt",html)
     Console.ReadKey() |> ignore
     
     0 // return an integer exit code
