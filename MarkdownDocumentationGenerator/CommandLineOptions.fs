b9b0ca856d1d842bfc02211ef6f64f9dbbf479d4
Alex McGilvray <alex@levelism.com>
2015-12-01 22:53:23 -0800

    Moving commandline options into its own file

---
+++ MarkdownDocumentationGenerator/CommandLineOptions.fs
@@ -0,0 +1,10 @@
+﻿module CommandLineOptions
+    
+    type OutputType = 
+        | Website       = 1 
+        | DocumentPDF   = 2 
+
+    type ProgramParameters = {
+        rootDocument    : string;
+        outputType      : OutputType;
+        }
--- MarkdownDocumentationGenerator/Program.fs
+++ MarkdownDocumentationGenerator/Program.fs
@@ -1,24 +1,16 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿module Program
 
+open CommandLineOptions
 open FSharp.Markdown
 open System
 
-module CommandLineParser = 
-    
-    type OutputType = 
-        | Website       = 1 
-        | DocumentPDF   = 2 
 
-    type ProgramParameters = {
-        rootDocument    : string;
-        outputType      : OutputType;
-        }
 
 [<EntryPoint>]
 let main argv = 
     let filteredArgs = argv.Length 
     Console.ReadKey() |> ignore
+    
     0 // return an integer exit code
 
 
