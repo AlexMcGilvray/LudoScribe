12461cb22d315e9c09b3454b3dc8c34b6cb2e39b
Alex McGilvray <alex@levelism.com>
2015-12-01 22:17:38 -0800

    First commit

---
+++ MarkdownDocumentationGenerator/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open FSharp.Markdown
+open System
+
+
+let alexTest = 4
+let alexTest2 = alexTest + 4
+
+
+[<EntryPoint>]
+let main argv = 
+    let filteredArgs = argv.Length
+    printfn "%A %A" alexTest2 filteredArgs
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
+
+
+
+
